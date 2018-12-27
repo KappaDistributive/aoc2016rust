bf65e4b5af8b43b30615ce3ddb7d2d70a78c51d9
Stefan Mesken <KappaDistributive@users.noreply.github.com>
2018-12-27 23:58:29 +0100

    solved part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -36,6 +36,43 @@ fn supports_tlp(input: &Vec<char>) -> bool {
     contains_abba(input)
 }
 
+fn supports_ssl(input: &str) -> bool {
+    let chars = input.chars().collect::<Vec<char>>();
+    let mut mask: Vec<u8> = Vec::new();
+    let mut x: u8 = 0;
+    for i in 0..chars.len() {
+        if chars[i] == ']' {
+            x = 0;
+        }
+        mask.push(x);
+        if chars[i] == '[' {
+            x = 1;
+        }
+    }
+
+    for i in 0..chars.len() - 2 {
+        for j in 0..chars.len() - 2 {
+            // chars[i..i+3] is ABA and char[j..j+3] is BAB and
+            // chars[i..i+3] is out and chars[j..j+3] is in
+            if chars[i] == chars[i + 2]
+                && chars[i] != chars[i + 1]
+                && chars[i] == chars[j + 1]
+                && chars[i + 1] == chars[j]
+                && chars[i + 1] == chars[j + 2]
+                && mask[i] == 0
+                && mask[i + 1] == 0
+                && mask[i + 2] == 0
+                && mask[j] == 1
+                && mask[j + 1] == 1
+                && mask[j + 2] == 1
+            {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn solve_part_1(input_str: &str) -> usize {
     let mut result: usize = 0;
     for line in input_str.lines() {
@@ -45,6 +82,17 @@ fn solve_part_1(input_str: &str) -> usize {
     }
     result
 }
+
+fn solve_part_2(input_str: &str) -> usize {
+    let mut result: usize = 0;
+    for line in input_str.lines() {
+        if supports_ssl(line) {
+            result += 1;
+        }
+    }
+    result
+}
 fn main() {
     println!("Answer part 1: {}", solve_part_1(INPUT));
+    println!("Answer part 2: {}", solve_part_2(INPUT));
 }
